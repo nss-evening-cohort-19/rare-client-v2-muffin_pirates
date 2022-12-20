24a4a79d2d6ff90dff577024b302aeb0d66a74e2
Kristen Chandler <krismchandler@gmail.com>
2022-12-19 18:32:53 -0600

    added the () to correctly call useAuth

--- pages/Posts/new.js
+++ pages/Posts/new.js
@@ -3,7 +3,7 @@ import PostForm from '../../components/forms/PostForm';
 import { useAuth } from '../../utils/context/authContext';
 
 export default function NewPost() {
-  const { user } = useAuth;
+  const { user } = useAuth();
   console.warn(user, 'user2');
   return (
     <div className="create-form" style={{ height: '45rem', padding: '10%' }}>
